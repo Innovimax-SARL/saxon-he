771722d8305a28859731191fb1bf970d75da540b
mike <mike@77040254-3fc5-46b0-8a78-b9049ce60247>
2017-06-21 09:24:49 +0000

    Updated version metadata
    git-svn-id: https://dev.saxonica.com/repos/archive/opensource/latest9.8@2091 77040254-3fc5-46b0-8a78-b9049ce60247

--- hej/net/sf/saxon/Version.java
+++ hej/net/sf/saxon/Version.java
@@ -13,10 +13,10 @@ package net.sf.saxon;
 
 public final class Version {
 
-    private static final int[] STRUCTURED_VERSION = {9, 8, 0, 1};
-    private static final String VERSION = "9.8.0.1";
-    private static final String BUILD = "060812"; //mmddhh
-    private static final String RELEASE_DATE = "2017-06-08";
+    private static final int[] STRUCTURED_VERSION = {9, 8, 0, 2};
+    private static final String VERSION = "9.8.0.2";
+    private static final String BUILD = "062110"; //mmddhh
+    private static final String RELEASE_DATE = "2017-06-21";
     private static final String MAJOR_RELEASE_DATE = "2017-06-08";
 
     private Version() {
