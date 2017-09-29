65cec248d2d49568ba2ac28cda2cace8f46819b5
ond1 <ond1@77040254-3fc5-46b0-8a78-b9049ce60247>
2017-09-29 12:13:46 +0000

    Update Saxon maintenance release date.
    git-svn-id: https://dev.saxonica.com/repos/archive/opensource/latest9.8@2297 77040254-3fc5-46b0-8a78-b9049ce60247

--- hej/net/sf/saxon/Version.java
+++ hej/net/sf/saxon/Version.java
@@ -15,8 +15,8 @@ public final class Version {
 
     private static final int[] STRUCTURED_VERSION = {9, 8, 0, 5};
     private static final String VERSION = "9.8.0.5";
-    private static final String BUILD = "092816"; //mmddhh
-    private static final String RELEASE_DATE = "2017-09-28";
+    private static final String BUILD = "092916"; //mmddhh
+    private static final String RELEASE_DATE = "2017-09-29";
     private static final String MAJOR_RELEASE_DATE = "2017-06-08";
 
     private Version() {
